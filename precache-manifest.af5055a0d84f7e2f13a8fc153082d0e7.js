4bb212cae3bf3b995e1c96c1d4262873dcd3e533
bds0900 <Dbeak9336@conestogac.on.ca>
2020-05-18 23:19:09 -0400

    Updates

---
+++ precache-manifest.af5055a0d84f7e2f13a8fc153082d0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976e3338accbb310fd2e845a58048ba0",
+    "url": "/comment/index.html"
+  },
+  {
+    "revision": "9f829fd2d3edc88c29f1",
+    "url": "/comment/static/css/main.be5a4477.chunk.css"
+  },
+  {
+    "revision": "c53ff9646b460c05c291",
+    "url": "/comment/static/js/2.3fcb3246.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/comment/static/js/2.3fcb3246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f829fd2d3edc88c29f1",
+    "url": "/comment/static/js/main.84deb27f.chunk.js"
+  },
+  {
+    "revision": "744920fc9da8de7cbbf0",
+    "url": "/comment/static/js/runtime-main.1c144add.js"
+  }
+]);
